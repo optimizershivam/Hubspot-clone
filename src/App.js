a497154cbb1431434720a3fcaf88bc8290f4c228
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-07-20 20:00:50 +0530

    footerAndbannerdone

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Banner from './components/Banner/Banner';
+import Footer from "./components/footer/Footer.jsx"
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Navbar/> */}
+     {/* <Footer/> */}
+     <Banner/>
+          <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,227 @@
+import {FaFacebookF} from "react-icons/fa";
+import {BsInstagram} from "react-icons/bs";
+import {BsYoutube} from "react-icons/bs";
+import {BsTwitter} from "react-icons/bs";
+import {FaLinkedinIn} from "react-icons/fa"
+
+
+export const Popularfeatures=[
+    {
+        name:"Free Meeting Scheduler App",
+        path:"/"
+    },
+    {
+        name:"Social Media Tools",
+        path:"/"
+    },
+    {
+        name:"Email Tracking Software",
+        path:"/"
+    },
+    {
+        name:"Sales Email Automation",
+        path:"/"
+    },
+    {
+        name:"Ads Software",
+        path:"/"
+    },
+    {
+        name:"Email Marketing Software",
+        path:"/"
+    },
+    {
+        name:"Lead Marketing Software",
+        path:"/"
+    },
+    {
+        name:"Pipline Management Tools",
+        path:"/"
+    },
+]
+
+
+export const features=[
+    {
+        name:"Sales Email Templates",
+        path:"/"
+    },
+    {
+        name:"Help Desk Software",
+        path:"/"
+    },
+    {
+        name:"Free Online Form Builder",
+        path:"/"
+    },
+    {
+        name:"Free Chatbot Builder",
+        path:"/"
+    },
+    {
+        name:"Free Live Chat Software",
+        path:"/"
+    },
+    {
+        name:"Marketing Analytics",
+        path:"/"
+    },
+    {
+        name:"Free Landing Page Builder",
+        path:"/"
+    }
+    
+   
+]
+
+
+export const freeTools=[
+    {
+        name:"Website Grader",
+        path:"/"
+    },
+    {
+        name:"Make My Persona",
+        path:"/"
+    },
+    {
+        name:"Email Signature Generator",
+        path:"/"
+    },
+    {
+        name:"Blog Ideas Generator",
+        path:"/"
+    },
+    {
+        name:"Invoice Template Generator",
+        path:"/"
+    },
+    {
+        name:"Marketing Plan Generator",
+        path:"/"
+    },
+    {
+        name:"Free Buisness Templates",
+        path:"/"
+    },
+    {
+        name:"Industry Benchmark Data",
+        path:"/"
+    },
+    {
+        name:"Software Comparisons Library",
+        path:"/"
+    },
+    {
+        name:"Website Themes & Templates",
+        path:"/"
+    },
+]
+
+
+
+export const Comapny=[
+    {
+        name:"About Us",
+        path:"/"
+    },
+    {
+        name:"Careers",
+        path:"/"
+    },
+    {
+        name:"Management Team",
+        path:"/"
+    },
+    {
+        name:"Board of Directors",
+        path:"/"
+    },
+    {
+        name:"Investor Relations",
+        path:"/"
+    },
+    {
+        name:"Blog",
+        path:"/"
+    },
+    {
+        name:"Contact Us",
+        path:"/"
+    }
+    
+]
+
+
+export const Costomers=[
+    {
+        name:"Customer Support",
+        path:"/"
+    },
+    {
+        name:"Join a Local User Group",
+        path:"/"
+    }     
+]
+
+
+export const Partners=[
+    {
+        name:"All Partner Programs",
+        path:"/"
+    },
+    {
+        name:"Solutions Partner Program",
+        path:"/"
+    },
+    {
+        name:"App Partner Program",
+        path:"/"
+    },
+    {
+        name:"HubSpot For Startups",
+        path:"/"
+    },
+    {
+        name:"Affiliate Program",
+        path:"/"
+    }
+    
+  
+    
+]
+
+
+
+export const socialIcons=[
+    {
+        icon:<FaFacebookF/>,
+        path:"/"
+    },
+    {
+        icon:<BsInstagram/>,
+        path:"/"
+    },
+    {
+        icon:<BsYoutube/>,
+        path:"/"
+    },
+    {
+        icon:<BsTwitter/>,
+        path:"/"
+    },
+    {
+        icon:<FaLinkedinIn/>,
+        path:"/"
+    }   
+]
+
+
+
+
+
+
+
+
+
+
