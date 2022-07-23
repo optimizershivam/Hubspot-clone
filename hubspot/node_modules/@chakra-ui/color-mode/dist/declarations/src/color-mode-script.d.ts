126b035056233fffb22c0da0e1a5a596c20950af
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-07-23 18:59:22 +0530

    nodeModuleRemove

--- hubspot/node_modules/@chakra-ui/color-mode/dist/declarations/src/color-mode-script.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare type ColorModeScriptProps = {
-    type?: "localStorage" | "cookie";
-    initialColorMode?: "light" | "dark" | "system";
-    storageKey?: string;
-};
-export declare function getScriptSrc(props?: ColorModeScriptProps): string;
-export declare function ColorModeScript(props?: ColorModeScriptProps): JSX.Element;
-//# sourceMappingURL=color-mode-script.d.ts.map
--- hubspot/node_modules/@chakra-ui/hooks/dist/declarations/src/use-mouse-down-ref.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-export declare function useMouseDownRef(shouldListen?: boolean): React.RefObject<HTMLElement>;
-//# sourceMappingURL=use-mouse-down-ref.d.ts.map
